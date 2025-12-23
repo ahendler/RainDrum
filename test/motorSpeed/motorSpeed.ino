d1c736f72a704f6de0b94e386968f93b36962075
Artur Hendler <arturhendler@hotmail.com>
2025-12-23 17:41:41 -0300

    Add test scripts

---
+++ test/motorSpeed/motorSpeed.ino
@@ -0,0 +1,69 @@
+#include <AccelStepper.h>
+
+// Hardware pins
+#define STEP_PIN 22
+#define DIR_PIN 23
+#define EN_PIN 19
+
+// Motor constants
+const int STEPS_PER_REV = 1600;
+const int half_rev = 800;
+const float MAX_RPM = 5000.0f;
+
+// Calculate max speed in steps/sec
+// (1600 steps/rev * 2000 rev/min) / 60 sec/min = 53333.33 steps/sec
+const float MAX_SPEED_STEPS = (STEPS_PER_REV * MAX_RPM) / 60.0;
+
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+
+float currentAcceleration = 20000.0; // Start with a reasonable acceleration
+float accelerationIncrement = 1000.0;
+float maxTestAcceleration = 60000.0; // Upper limit to stop the test
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  pinMode(EN_PIN, OUTPUT);
+  digitalWrite(EN_PIN, LOW); // Enable motor (Active LOW)
+
+  stepper.setMaxSpeed(MAX_SPEED_STEPS);
+  
+  Serial.println("Motor Acceleration Test");
+  Serial.println("-----------------------");
+  Serial.print("Max Speed: "); Serial.println(MAX_SPEED_STEPS);
+  Serial.println("Starting test in 3 seconds...");
+  delay(3000);
+}
+
+void loop() {
+  if (currentAcceleration <= maxTestAcceleration) {
+    Serial.print("Testing Acceleration: ");
+    Serial.println(currentAcceleration);
+
+    stepper.setAcceleration(currentAcceleration);
+
+    // Move 1 revolution forward
+    stepper.move(half_rev);
+    while (stepper.distanceToGo() != 0) {
+      stepper.run();
+    }
+    
+    delay(200); // Short pause
+
+    // Move 1 revolution backward
+    stepper.move(-half_rev);
+    while (stepper.distanceToGo() != 0) {
+      stepper.run();
+    }
+
+    delay(500); // Pause before next iteration
+
+    // Increase acceleration for next run
+    currentAcceleration += accelerationIncrement;
+  } else {
+    Serial.println("Test sequence complete.");
+    digitalWrite(EN_PIN, HIGH); // Disable motor
+    while(1); // Stop
+  }
+}
---
+++ test/solenoidSpeed/solenoidSpeed.ino
@@ -0,0 +1,27 @@
+#define STICK_A 18
+#define STICK_B 17
+
+void setup() {
+    pinMode(STICK_A, OUTPUT);
+    digitalWrite(STICK_A, HIGH);
+    
+    pinMode(STICK_B, OUTPUT);
+    digitalWrite(STICK_B, HIGH);
+    Serial.begin(115200);
+    Serial.println("Starting Solenoid Speed Test");
+}
+
+void loop() {
+    //Test how many ms it takes to strike the solenoid
+    for (int delayMs = 14; delayMs <= 30; delayMs += 2){
+        Serial.println("Striking with delay: " + String(delayMs) + " ms");
+        for (int i = 0; i < 6; i++) {
+            digitalWrite(STICK_A, LOW);
+            delay(delayMs);
+            digitalWrite(STICK_A, HIGH);
+            delay(500); // Wait .5 second between strikes
+        }
+        Serial.println("");
+        delay(2000); // Wait 2 seconds between different delays
+    }
+}

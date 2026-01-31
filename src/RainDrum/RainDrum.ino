2751560d04d6721fe1df6e06087993867c08670f
Artur Hendler <arturhendler@hotmail.com>
2026-01-30 21:38:07 -0300

    Downgrade core version and add note

--- src/RainDrum/RainDrum.ino
+++ src/RainDrum/RainDrum.ino
@@ -3,6 +3,8 @@
 #define ARDUINO_USB_CDC_ON_BOOT 1
 #define ARDUINO_USB_MODE 1
 
+// MidiBle is only supported on espressif/arduino-esp32 core version 2.0.17 and below
+
 MidiBle midiListener;
 Drum drumPlayer;
 
